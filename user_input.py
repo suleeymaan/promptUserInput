919772d31f5e2c249e0538f18e7a78a747b748de
Suleiman Abdinasir <134613319+suleeymaan@users.noreply.github.com>
2024-02-26 21:29:55 +0300

    First Commit

---
+++ user_input.py
@@ -0,0 +1,8 @@
+#Ask the user for their name and store it in a variable called "name".
+name=input("What is your name?")
+#Ask the user for their age and store it in a variable called "age".
+age=input("How old are you?")
+#Ask the user for their location and store it in a variable called "location".
+location=input("Where are you located?")
+#Print out a personalized message using the user's name, age, and location.
+print("Hello, "+name+"! You are "+age+" years old and live in "+location+". It is nice to meet you.")
